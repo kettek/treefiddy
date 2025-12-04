eb92352b75defc73ae43ec6d53020ac09b31e054
kettek <kts@kettek.net>
2025-12-03 21:48:14 -0800

    Add git edict plugin

---
+++ plugins/js/git-edict/index.ts
@@ -0,0 +1,29 @@
+import type { EdictContext, Mangled, Plugin } from '../treefiddy'
+
+interface LocalPlugin {
+}
+
+const plugin: Plugin & LocalPlugin = {
+	permissions: {
+		exec: ['git'],
+	},
+	exec: (cmd: string, ...args: string[]): string => {
+		return ''
+	},
+	edicts: {
+		"git": (ctx: EdictContext): EdictContext => {
+			switch (ctx.Arguments[0]) {
+				case 'stage':
+				case 'add':
+					plugin.exec?.('git', 'add', ctx.Selected)
+					break
+				case 'unstage':
+					plugin.exec?.('git', 'restore', '--staged', ctx.Selected)
+					break
+			}
+			return ctx
+		},
+	},
+}
+
+export default plugin
