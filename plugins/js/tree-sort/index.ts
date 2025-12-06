e9aa04b82d5d50b175a63167a8b04ee1e663398f
kettek <kts@kettek.net>
2025-12-06 02:37:37 -0800

    Add "directoriesFirst" sort option

--- plugins/js/tree-sort/index.ts
+++ plugins/js/tree-sort/index.ts
@@ -1,11 +1,22 @@
-import type { FileReference, Plugin } from '../treefiddy'
+import type { Config, FileReference, Plugin } from '../treefiddy'
 
-const plugin: Plugin = {
+interface LocalPlugin {
+	config: Config & {
+		directoriesFirst: boolean
+	}
+}
+
+const plugin: Plugin & LocalPlugin = {
+	config: {
+		directoriesFirst: true,
+	},
 	sortTreeNode: function (a: FileReference, b: FileReference): number {
-		if (a.Dir && !b.Dir) {
-			return -1
-		} else if (!a.Dir && b.Dir) {
-			return 1
+		if (plugin.config.directoriesFirst) {
+			if (a.Dir && !b.Dir) {
+				return -1
+			} else if (!a.Dir && b.Dir) {
+				return 1
+			}
 		}
 		const an = a.OriginalName.toLowerCase()
 		const bn = b.OriginalName.toLowerCase()
